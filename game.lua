40a97a7254a71b14ac6696ac02e300b3b6800009
kloki <k.klinkers@gmail.com>
2013-03-05 23:00:59 +0100

    start menu working beginning game itself

---
+++ game.lua
@@ -0,0 +1,31 @@
+-- kloki
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+--
+-- Koen Klinkers k.klinkers@gmail.com
+
+game={
+   background=love.graphics.newImage("images/background.png")
+   figure=love.graphics.newImage("images/figure.png")
+   time=0
+   counter=150
+}
+
+
+function game:update(dt)
+
+end
+
+function game:draw()
+end
